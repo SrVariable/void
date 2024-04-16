59e53973ebd6fb48da764ef3c5960b84f376939d
SrRecursive <rojohnibana@gmail.com>
2024-04-16 23:11:31 +0200

    Fix issues on Mac

--- trolling_the_troll/example_trolling_the_troll_bonus.c
+++ trolling_the_troll/example_trolling_the_troll_bonus.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   example_trolling_the_troll.c                       :+:      :+:    :+:   */
+/*   example_trolling_the_troll_bonus.c                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 07:05:36 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2024/04/16 22:17:05 by ribana-b         ###   ########.com      */
+/*   Updated: 2024/04/16 22:21:15 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "trolling_the_troll.h"
+#include "trolling_the_troll_bonus.h"
 #include <stdio.h>
 
 int	main(void)
 {
 	char	s[] = "Hello World";
 	char	c = '#';
+	char	**dict = ft_split("World", ' ');
+	if (!dict)
+		return (1);
 
 	printf("Original string: %s\n", s);
-	printf("Modified string: %s\n", trolling_the_troll(s, c));
+	printf("Modified string: %s\n", trolling_the_troll(s, c, dict));
+	ft_free(&dict, 2);
 	return (0);
 }
