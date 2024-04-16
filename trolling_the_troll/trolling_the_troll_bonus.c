561a9ed1867ea7a1aa60dc3676a545f51bbd5bbb
SrVariable <rojohnibana@gmail.com>
2024-04-16 13:35:54 +0200

    Refactor and fix bonus

--- trolling_the_troll/trolling_the_troll_bonus.c
+++ trolling_the_troll/trolling_the_troll_bonus.c
@@ -1,26 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   trolling_the_troll.c                               :+:      :+:    :+:   */
+/*   trolling_the_troll_bonus.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/28 20:57:45 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2024/04/16 12:14:58 by ribana-b         ###   ########.com      */
+/*   Created: 2024/04/16 12:15:04 by ribana-b          #+#    #+# Malaga      */
+/*   Updated: 2024/04/16 12:55:12 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "trolling_the_troll.h"
+#include "trolling_the_troll_bonus.h"
 
-char	*trolling_the_troll(char *str, char new_char)
+char	*trolling_the_troll(char *str, char new_char, char **dict)
 {
 	int	i;
+	int	j;
 
-	i = -1;
-	if (!str)
+	if (!dict || !str)
 		return (NULL);
+	i = -1;
 	while (str[++i])
-		if (ft_strchr("aeiouAEIOU", str[i]))
-			str[i] = new_char;
+	{
+		j = -1;
+		while (dict[++j])
+		{
+			if (ft_strncmp(&str[i], dict[j], ft_strlen(dict[j])) == 0)
+			{
+				while (str[i] && str[i] != ' ')
+				{
+					if (ft_strchr("aeiouAEIOU", str[i]))
+						str[i] = new_char;
+					++i;
+				}
+			}
+		}
+	}
 	return (str);
 }
--- trolling_the_troll/trolling_the_troll_bonus.h
+++ trolling_the_troll/trolling_the_troll_bonus.h
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   trolling_the_troll.h                               :+:      :+:    :+:   */
+/*   trolling_the_troll_bonus.h                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 06:09:02 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2024/04/16 12:18:16 by ribana-b         ###   ########.com      */
+/*   Updated: 2024/04/16 12:36:03 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TROLLING_THE_TROLL_H
-# define TROLLING_THE_TROLL_H
+#ifndef TROLLING_THE_TROLL_BONUS_H
+# define TROLLING_THE_TROLL_BONUS_H
 
 # include "BFL.h"
 
-char	*trolling_the_troll(char *str, char new_char);
+char	*trolling_the_troll(char *str, char new_char, char **dict);
 
-#endif // TROLLING_THE_TROLL_H
+#endif // TROLLING_THE_TROLL_BONUS_H
